f4d5e0869753436060d9042346a323e46544ca80
zjzjzj <zhangjing@mifanli.com>
2017-02-07 19:26:06 +0800

    fixed

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -5,6 +5,7 @@
  * Date: 2016/12/16
  * Time: 11:31
  */
+date_default_timezone_set('Asia/Shanghai');   //设置时区
 session_start();
 header('Content-type:text/html;charset=utf-8');
 define('DB_HOST','localhost');
--- member_photo_update.php
+++ member_photo_update.php
@@ -5,8 +5,69 @@
  * Date: 2017/2/5
  * Time: 16:26
  */
-
+include_once "inc/config.inc.php";
+include_once "inc/mysql.inc.php";
+include_once "inc/tool.inc.php";
+include_once "inc/upload.inc.php";
+$link=connect();
+if(!$member_id=is_login($link)){
+    skip('login.php','error','请登录后再对自己的头像做设置！');
+}
+if (isset($_POST['submit'])){
+    $save_path='uploads'.date('/Y/m/d');
+    $upload=upload($save_path,'2M','photo');
+    if ($upload['return']){
+        echo '成功！';
+    }else{
+        skip('member_photo_update.php','error',$upload['error']);
+    }
+}
 ?>
-<?php include_once "inc/header.inc.php"?>
-
-<?php include_once "inc/footer.inc.php"?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <style type="text/css">
+        body {
+            font-size:12px;
+            font-family:微软雅黑;
+        }
+        h2 {
+            padding:0 0 10px 0;
+            border-bottom: 1px solid #e3e3e3;
+            color:#444;
+        }
+        .submit {
+            background-color: #3b7dc3;
+            color:#fff;
+            padding:5px 22px;
+            border-radius:2px;
+            border:0px;
+            cursor:pointer;
+            font-size:14px;
+        }
+        #main {
+            width:80%;
+            margin:0 auto;
+        }
+    </style>
+</head>
+<body>
+<div id="main">
+    <h2>更改头像</h2>
+    <div>
+        <h3>原头像：</h3>
+        <img src="style/photo.jpg" />
+    </div>
+    <div style="margin:15px 0 0 0;">
+        <form method="post" enctype="multipart/form-data">
+            <input style="cursor:pointer;" width="100" name="photo" type="file" /><br /><br />
+            <input class="submit" type="submit" name="submit" value="保存" />
+        </form>
+    </div>
+</div>
+</body>
+</html>
